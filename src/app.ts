d3de8c457de2a3c893e8883971314a83b42f6da9
Felix Rodriguez <= rodriguezfelix17@gmail.com>
2023-04-21 15:02:47 -0400

    adding cors

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,15 @@
 import express, { Express, Request, Response } from 'express';
 import routersV1 from './routes/Api/V1/index'
+import cors from 'cors';
 
 const app: Express = express();
 
-app.use(express.json())
+app.use(express.json());
+var corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200,
+}
+app.use(cors(corsOptions));
 
 app.get('/', (req: Request, res: Response) => {
     res.send('hello world, testing');
